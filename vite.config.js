dce153af8cb0a53ef7b8cf58c2af6a8acbfd48b4
chengzx1986 <chengzx1986@gmail.com>
2026-02-07 12:21:33 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -7,5 +7,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/ChengZhiXiang-Capstone/ChengZhiXiang-Capstone-App/',
+  base: '/ChengZhiXiang-Capstone/',
 })
